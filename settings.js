dd555da28efcb808eb8257943a68fd05789aa5ba
KingZord263 <kingzord263@gmail.com>
2025-05-02 16:50:47 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,15 @@
+// DONT CHANGE ANYTHING HERE OTHERWISE THE BOT WONT RUN?
+
+const settings = {
+  packname: 'PARSLEY',
+  author: 'ᴩᴏᴡᴇʀᴇᴅ ʙy ᴛʜʀɪ_ɪꜱ_ᴋɪᴅ',
+  botName: "PARSLEY-XMD",
+  botOwner: 'THRI_IZ_KIDD, // Your name
+  ownerNumber: '+263776404156', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.0",
+};
+
+module.exports = settings;
